d6b5bca972e75049ff38843d0fb57719d3112541
rpimentajr <148414437+rpimentajr@users.noreply.github.com>
2026-07-12 23:02:43 -0300

    Implement async setup and unload for UniFi LED

---
+++ custom_components/ unifi_led/custom_components/unifi_led/__init__.py
@@ -0,0 +1,51 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import DOMAIN
+from .coordinator import UniFiLedCoordinator
+
+
+async def async_setup(hass, config):
+
+    hass.data.setdefault(DOMAIN, {})
+
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry
+):
+
+    coordinator = UniFiLedCoordinator(
+        hass,
+        entry
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        ["button", "sensor"]
+    )
+
+    return True
+
+
+async def async_unload_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry
+):
+
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry,
+        ["button", "sensor"]
+    )
+
+    if unload_ok:
+
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
