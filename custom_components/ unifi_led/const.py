31c82db601bddf8f00178d775a19db7810975395
rpimentajr <148414437+rpimentajr@users.noreply.github.com>
2026-07-12 23:02:06 -0300

    Add constants for unifi_led integration
    Define constants for unifi_led integration including domain, platforms, default port, configuration keys, service name, and color mappings.

---
+++ custom_components/ unifi_led/const.py
@@ -0,0 +1,26 @@
+DOMAIN = "unifi_led"
+
+PLATFORMS = [
+    "button",
+    "sensor"
+]
+
+DEFAULT_PORT = 22
+
+CONF_HOST = "host"
+CONF_USERNAME = "username"
+CONF_PRIVATE_KEY = "private_key"
+
+SERVICE_SET_COLOR = "set_color"
+
+COLORS = {
+
+    "blue": "1,23,103",
+
+    "green": "0,255,0",
+
+    "red": "255,0,0",
+
+    "white": "255,255,255"
+
+}
